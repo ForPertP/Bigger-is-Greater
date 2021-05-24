4f8e6e7b00b827441e04038b3d6ecf8d2f93aced
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-05-24 15:51:27 +0900

    Update Bigger-is-Greater.cpp

--- Bigger-is-Greater.cpp
+++ Bigger-is-Greater.cpp
@@ -1,5 +1,27 @@
 string biggerIsGreater(string w)
 {
+    string result{};
+    size_t n = w.length();
+
+    for (int i = n-2; i >= 0; --i)
+    {
+        for (int j = n-1; j > i; --j)
+        {
+            if (w[i] < w[j])
+            {
+                std::swap(w[i], w[j]);
+                std::sort(w.begin()+i+1, w.end());
+                return w;
+            }
+        }
+    }
+
+    return "no answer";
+}
+
+
+string biggerIsGreater1(string w)
+{
     if (next_permutation(w.begin(), w.end()))
     {
         return w;
