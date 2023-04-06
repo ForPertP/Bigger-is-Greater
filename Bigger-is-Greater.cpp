77bcee0d712a6b09eb374bf6b7e853deb7f3e65b
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-04-07 06:18:07 +0900

    Update Bigger-is-Greater.cpp

--- Bigger-is-Greater.cpp
+++ Bigger-is-Greater.cpp
@@ -1,3 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'biggerIsGreater' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING w as parameter.
+ */
+
 string biggerIsGreater(string w)
 {
     string result{};
